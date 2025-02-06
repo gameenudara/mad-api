12db9e25e83bf7c00777c9d40ddbf362863b1c85
gameenudara <gameenudara567@gmail.com>
2025-02-07 01:12:27 +0530

    first commit

---
+++ src/main/java/lk/futuresolution/mad/api/MadApiApplication.java
@@ -0,0 +1,13 @@
+package lk.futuresolution.mad.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/futuresolution/mad/api/MadApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.futuresolution.mad.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
